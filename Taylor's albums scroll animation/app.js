756929b70780df0aa292d3343b23ef4c2239d774
carocomba28 <carolinacomba42@gmail.com>
2024-04-05 18:58:15 -0300

    indentado

--- Taylor's albums scroll animation/app.js
+++ Taylor's albums scroll animation/app.js
@@ -5,15 +5,15 @@ window.addEventListener('scroll', aparecerBoxes);
 aparecerBoxes();
 
 function aparecerBoxes() {
-    const trigger = (window.innerHeight / 5) * 4;
+  const trigger = (window.innerHeight / 5) * 4;
 
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top;
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
 
-        if (boxTop < trigger) {
-            box.classList.add('show');
-        } else {
-            box.classList.remove('show');
-        }
-    });
+    if (boxTop < trigger) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
 }
